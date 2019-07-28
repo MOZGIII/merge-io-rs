85aa56bc67337ba3f8b462bd29fdd9e570b91350
MOZGIII <mike-n@narod.ru>
2019-07-28 21:53:03 +0300

    Rewrite test to fully reside at async context

--- tests/test.rs
+++ tests/test.rs
@@ -4,34 +4,34 @@
 use futures::executor;
 use futures::{AsyncReadExt, AsyncWriteExt};
 
-use std::io::{Cursor, Result};
 use merge_io::MergeIO;
+use std::io::{Cursor, Result};
 
 #[test]
 fn test_duplex() -> Result<()> {
-    let read_sample = vec![1, 2, 3, 4];
-    let write_sample = vec![10, 20, 30, 40];
-
-    let reader = Cursor::new(read_sample.clone());
-    let writer = Cursor::new(vec![0u8; 1024]);
-    let mut tio = MergeIO::new(reader, writer);
-
     executor::block_on(async {
-        tio.write_all(&write_sample).await.unwrap();
+        let read_sample = vec![1, 2, 3, 4];
+        let write_sample = vec![10, 20, 30, 40];
+
+        let reader = Cursor::new(read_sample.clone());
+        let writer = Cursor::new(vec![0u8; 1024]);
+        let mut tio = MergeIO::new(reader, writer);
+
+        tio.write_all(&write_sample).await?;
 
         let mut read_buf = Vec::<u8>::with_capacity(1024);
-        tio.read_to_end(&mut read_buf).await.unwrap();
+        tio.read_to_end(&mut read_buf).await?;
 
         assert_eq!(&read_buf, &read_sample);
-    });
 
-    let (outcome_read_buf, outcome_write_buf) = tio.into_inner();
+        let (outcome_read_buf, outcome_write_buf) = tio.into_inner();
 
-    assert_eq!(outcome_read_buf.position(), read_sample.len() as u64);
-    assert_eq!(
-        outcome_write_buf.into_inner()[..write_sample.len()],
-        write_sample[..]
-    );
+        assert_eq!(outcome_read_buf.position(), read_sample.len() as u64);
+        assert_eq!(
+            outcome_write_buf.into_inner()[..write_sample.len()],
+            write_sample[..]
+        );
 
-    Ok(())
+        Ok(())
+    })
 }
